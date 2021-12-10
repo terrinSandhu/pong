eb44d46203065aa27b31fe1243054fd8075b6673
terrinSandhu <63380539+terrinSandhu@users.noreply.github.com>
2021-12-10 12:25:44 -0800

    added img

--- diagnostic.py
+++ diagnostic.py
@@ -1,1 +1,10 @@
-import pyautogui as pg
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+iris = sns.load_dataset('iris')
+
+length_plot = sns.barplot(x='sepal_length', y='species', data=iris)
+plt.savefig('ex1.pdf')
+plt.clf()
+width_plot = sns.barplot(x='sepal_width', y='species', data=iris)
+plt.savefig('ex2.pdf')
--- graphing.py
+++ graphing.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plt
 import pyautogui as pg
 import cv2 as cv
+from PIL import Image
 
-r = .1
-n =  4   
+img_path = "C:\\Users\\tssan\\Documents\\GitHub\\pong\\images.jpg"
 
+im = Image.open(img_path)
+width, height = im.size
+if width > height:
+    height = width
+
+
+n =  4 
+r = abs(height/(n*2))
+  
 x0 = r
 y0 = (2*n-1)*r
 
-fig, ax = plt.subplots()        #init plots
+
+fig, ax = plt.subplots()  
+img = plt.imread(img_path)
 
 while n > 0:
     x = x0
@@ -21,6 +32,6 @@ while n > 0:
     y0-= (2*r)
     n-=1
 
-
-
+ax.imshow(img)
+plt.axis("off")
 plt.show()
